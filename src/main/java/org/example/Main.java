eb8bf389625fbe8ce664f21c7054ce39b13cc52d
bhavika <bhauarage@gmail.com>
2024-04-25 14:06:31 +0530

    moved update and draw ellipse into ball class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,18 +7,7 @@ public class Main extends PApplet {
     public static final int WIDTH = 648;
     public static final int HEIGHT = 488;
     public static final int DIAMETER = 10;
-    class Ball{
-        int x,increment;
-        float height;
 
-
-        public Ball(int increment,float height) {
-            this.increment = increment;
-            this.height=height;
-            this.x=0;
-        }
-    }
-    
     Ball b1,b2,b3,b4;
     public static void main(String[] args) {
         PApplet.main("org.example.Main",args);
@@ -33,31 +22,39 @@ public class Main extends PApplet {
     @Override
     public void setup() {
 //        ellipse(WIDTH/2,HEIGHT/2, DIAMETER, DIAMETER);
-        b1=new Ball(1,0.2F);
-        b2=new Ball(2,0.4F);
-        b3=new Ball(3,0.6F);
-        b4=new Ball(4,0.8F);
+        b1= new Ball(1, 0.2F);
+        b2= new Ball(2, 0.4F);
+        b3= new Ball(3, 0.6F);
+        b4= new Ball(4, 0.8F);
     }
 
     @Override
     public void draw() {
-        drawEllipse(b1);
-        drawEllipse(b2);
-        drawEllipse(b3);
-        drawEllipse(b4);
-
-        update(b1);
-        update(b2);
-        update(b3);
-        update(b4);
+        b1.drawEllipse();
+        b2.drawEllipse();
+        b3.drawEllipse();
+        b4.drawEllipse();
     }
 
-    private void update(Ball b) {
-        b.x= b.x+ b.increment;
-    }
+     class Ball{
+        private int x,increment;
+        private float height;
 
-    private void drawEllipse(Ball obj) {
-        ellipse(obj.x, HEIGHT* obj.height, DIAMETER, DIAMETER);
-    }
 
+        public Ball(int increment,float height) {
+            this.increment = increment;
+            this.height=height;
+            this.x=0;
+        }
+
+        private void update() {
+            x= x+ increment;
+        }
+
+        private void drawEllipse() {
+            ellipse(x, HEIGHT* height, DIAMETER, DIAMETER);
+            update();
+        }
+    }
 }
+
